3eb96271e74789ed2344f49af03c9856a1d39c76
eavladimirov <aaa@aaa.bg>
2015-08-19 02:18:59 +0300

    Added methods
    Added methods Screen, validateSelection, Hand

--- RockPaperScissors/RPSgame.cs
+++ RockPaperScissors/RPSgame.cs
@@ -11,6 +11,39 @@ namespace RockPaperScissors
         public enum Hand {Rock = 1, Paper, Scissors};
         public enum Outcome  {Win, Lose, Tie};
 
-        public Hand ComputerHand
+        public Hand ComputerHand { get; set; }
+        public Hand PlayerHand { get; set; }
+        public char PlayerSelection { get; set;}
+
+        public Hand getUserHand()
+        {
+            while (!validateSelection)
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid input");
+                Screen();
+                PlayerSelection = Convert.ToChar(Console.ReadLine());
+            }
+        }
+
+        private bool validateSelection()
+        {
+            char value = Char.ToUpper(PlayerSelection);
+            
+            if(value != 'R' && value != 'P' && value != 'S')
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void Screen()
+        {
+            Console.WriteLine("Rock - R");
+            Console.WriteLine("Papper - P");
+            Console.WriteLine("Scissers - S");
+            Console.WriteLine("Please chose:");
+        }
     }
 }
