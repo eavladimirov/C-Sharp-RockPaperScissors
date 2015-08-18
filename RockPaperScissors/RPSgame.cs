97e1e879966f985b97dba6f1b5cb44fc90669107
eavladimirov <aaa@aaa.bg>
2015-08-19 01:59:02 +0300

    CreateProject

---
+++ RockPaperScissors/RPSgame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class RPSgame
+    {
+        public enum Hand {Rock = 1, Paper, Scissors};
+        public enum Outcome  {Win, Lose, Tie};
+
+        public Hand ComputerHand
+    }
+}
