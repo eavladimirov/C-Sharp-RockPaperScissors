96bf21a5d25e0e9827d67ef8449e86ee9183a042
eavladimirov <aaa@aaa.bg>
2015-08-19 16:11:43 +0300

    PlayGame method was added

--- RockPaperScissors/RPSgame.cs
+++ RockPaperScissors/RPSgame.cs
@@ -8,7 +8,7 @@ namespace RockPaperScissors
 {
     class RPSgame
     {
-        public enum Hand {Rock = 1, Paper, Scissors};
+        public enum Hand {Rock = 1, Papper, Scissors};
         public enum Outcome  {Win, Lose, Tie};
 
         public Hand ComputerHand { get; set; }
@@ -27,11 +27,13 @@ namespace RockPaperScissors
                 switch (Char.ToUpper(PlayerSelection))
                 {
                     case 'R': PlayerHand = Hand.Rock; break;
-                    case 'P': PlayerHand = Hand.Paper; break;
+                    case 'P': PlayerHand = Hand.Papper; break;
                     case 'S': PlayerHand = Hand.Scissors; break;
                     default: throw new Exception("Unexpected Error");
                 }
             }
+
+            return PlayerHand;
         }
 
         private bool validateSelection()
@@ -44,6 +46,75 @@ namespace RockPaperScissors
             }
             
             return true;
+        }
+
+        public void PlayGame()
+        {
+            bool gameOver = false;
+            var rand = new Random();
+            char response;
+
+            while (!gameOver)
+            {
+                Screen();
+                PlayerSelection = Convert.ToChar(Console.ReadLine());
+                getUserHand();
+                ComputerHand = (Hand)rand.Next(1, 4);
+                Console.Clear();
+                Console.WriteLine("Computer's hand: {0}", ComputerHand);
+                Console.WriteLine("Player's hand: {0}", PlayerHand);
+
+                if (DeterminateWinner() == Outcome.Win)
+                {
+                    Console.WriteLine("{0} beats {1}", PlayerHand, ComputerHand);
+                    Console.WriteLine("Player wins.");
+                }
+                else if (DeterminateWinner() == Outcome.Lose)
+                {
+                    Console.WriteLine("{0} beats {1}", ComputerHand, PlayerHand);
+                    Console.WriteLine("Player lose.");
+                }
+                else
+                {
+                    Console.WriteLine("It's a tie.");
+                }
+
+                Console.WriteLine("Another game (y/n)?");
+                response = Convert.ToChar(Console.ReadLine());
+
+
+            }
+             
+        }
+
+        public Outcome DeterminateWinner()
+        {
+            if (PlayerHand == Hand.Rock && ComputerHand == Hand.Scissors)
+            {
+                return Outcome.Win;
+            }
+            else if (PlayerHand == Hand.Papper && ComputerHand == Hand.Rock)
+            {
+                return Outcome.Win;
+            }
+            else if (PlayerHand == Hand.Scissors && ComputerHand == Hand.Papper)
+            {
+                return Outcome.Win;
+            }
+            else if (PlayerHand == Hand.Rock && ComputerHand == Hand.Papper)
+            {
+                return Outcome.Lose;
+            }
+            else if (PlayerHand == Hand.Papper && ComputerHand == Hand.Scissors)
+            {
+                return Outcome.Lose;
+            }
+            else if (PlayerHand == Hand.Scissors && ComputerHand == Hand.Rock)
+            {
+                return Outcome.Lose;
+            }
+
+            return Outcome.Tie;
         }
 
         private void Screen()
